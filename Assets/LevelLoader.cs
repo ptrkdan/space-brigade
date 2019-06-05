37844d49dd0aa31f83e4fa3535b4e2fbe166a00d
Patrick Dan Lacuna <ptrkdan@gmail.com>
2019-06-05 20:49:03 +0900

    - Implemented rough Level scene UI

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    // TODO Refactor
+    public void GoToLevel()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void GoToOptions()
+    {
+        Debug.Log("Options scene loaded");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
+
