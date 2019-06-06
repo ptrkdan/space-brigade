f514e9f51fc5f6b2358e7e766fcbec7f7ea7f3c8
Patrick Dan Lacuna <ptrkdan@gmail.com>
2019-06-06 11:29:38 +0900

    - Added standard defender unit, Laser Turret - Added idle animation and attacking animation for Laser Turret

---
+++ Assets/Scripts/Common/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.GetComponent<Health>();
+        var attacker = other.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            // health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Shooter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+
+    [SerializeField] Projectile projectilePrefab;
+    [SerializeField] GameObject weaponPos;
+
+    GameObject projectileParent;
+
+    private void Start()
+    {
+        SetProjectileParent();
+    }
+
+    private void SetProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
+
+    public void Fire()
+    {
+        Debug.Log($"{gameObject.name} fires projectile");
+        Projectile newProjectile = Instantiate(projectilePrefab, weaponPos.transform.position, weaponPos.transform.rotation);
+        newProjectile.transform.parent = projectileParent.transform;
+    }
+}
